3a13ad55127d686ef479a151edd99099dcce64aa
Mark <mark@markg.co.uk>
2020-12-03 17:27:28 +0000

    Refactor day 3

--- src/Day3.java
+++ src/Day3.java
@@ -8,64 +8,51 @@ public class Day3 {
         System.out.println(part2(list));
     }
 
-    public static int part1(List<String> list) {
-        int treeCount = 0;
+    public static long part1(List<String> list) {
+        return countTrees(new Slope(3, 1), list);
+    }
+
+    public static long part2(List<String> list) {
+        var slopes = initSlopes();
+        long totalCount = 1;
+        for (Slope slope : slopes) {
+            totalCount *= countTrees(slope, list);
+        }
+        return totalCount;
+    }
+
+    public static long countTrees(Slope slope, List<String> list) {
+        int x = 0;
         int width = list.get(0).length();
-        int xCord = 0;
-        for (String string : list) {
-            if (string.equals(list.get(0))) {
-                continue;
+        int treeCount = 0;
+        for (int i = slope.y; i < list.size(); i += slope.y) {
+            x += slope.x;
+            if (x >= width) {
+                x = x % width;
             }
-            xCord += 3;
-            if (xCord >= width) {
-                xCord = xCord % width;
-            }
-            if (string.charAt(xCord) == '#') {
+            if (list.get(i).charAt(x) == '#') {
                 treeCount++;
             }
         }
         return treeCount;
     }
 
-    public static long part2(List<String> list) {
-        var pairs = initPairs();
-        int width = list.get(0).length();
-        long totalCount = 0;
-
-        for (Pair pair : pairs) {
-            int xCord = 0;
-            int treeCount = 0;
-            for (int i = pair.y; i < list.size(); i += pair.y) {
-                xCord += pair.x;
-                if (xCord >= width) {
-                    xCord = xCord % width;
-                }
-                if (list.get(i).charAt(xCord) == '#') {
-                    treeCount++;
-                }
-            }
-            totalCount = totalCount == 0 ? treeCount : totalCount * treeCount;
-        }
-
-        return totalCount;
+    private static List<Slope> initSlopes() {
+        List<Slope> slopes = new ArrayList<>();
+        slopes.add(new Slope(1, 1));
+        slopes.add(new Slope(3, 1));
+        slopes.add(new Slope(5, 1));
+        slopes.add(new Slope(7, 1));
+        slopes.add(new Slope(1, 2));
+        return slopes;
     }
 
-    private static List<Pair> initPairs() {
-        List<Pair> pairs = new ArrayList<>();
-        pairs.add(new Pair(1, 1));
-        pairs.add(new Pair(3, 1));
-        pairs.add(new Pair(5, 1));
-        pairs.add(new Pair(7, 1));
-        pairs.add(new Pair(1, 2));
-        return pairs;
-    }
-
-    private static class Pair {
+    private static class Slope {
 
         public int x;
         public int y;
 
-        public Pair(int x, int y) {
+        public Slope(int x, int y) {
             this.x = x;
             this.y = y;
         }
